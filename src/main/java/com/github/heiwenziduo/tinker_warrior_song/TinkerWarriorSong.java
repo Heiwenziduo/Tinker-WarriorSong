34fdb129fcdff0167761a2319a68a99f2fcd85bc
🐜 <125963716+Heiwenziduo@users.noreply.github.com>
2025-09-13 17:01:56 +0800

    init from zenith

---
+++ src/main/java/com/github/heiwenziduo/tinker_warrior_song/TinkerWarriorSong.java
@@ -0,0 +1,39 @@
+package com.github.heiwenziduo.tinker_warrior_song;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TinkerWarriorSong.ModId)
+public class TinkerWarriorSong {
+    // Define mod id in a common place for everything to reference
+    public static final String ModId = "tinker_warrior_song";
+
+    public TinkerWarriorSong(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = ModId, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            // Some client setup code
+        }
+    }
+}
